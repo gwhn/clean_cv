134106827b56e06724763d8a2c3d659859f4cc28
Guy Newman <guy@weblitz.co.uk>
2010-07-06 15:58:14 +0100

    changed some styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
+  caches_page :index
+
   def index
     @person = Person.find(:first, :include => [:companies, :skills, :schools])
   end
-
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,7 @@ class Person < ActiveRecord::Base
   validates_format_of :portrait_url, :with => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for GIF, JPG or PNG image'
 
-  has_many :companies
+  has_many :companies, :order => 'start_date DESC'
   has_many :skills
-  has_many :schools
+  has_many :schools, :order => 'date_from DESC'
 end
