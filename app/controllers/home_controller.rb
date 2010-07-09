e359e098ba62ae79f1de4819027076fbca5ad140
Guy Newman <guy@weblitz.co.uk>
2010-07-09 10:52:55 +0100

    add action caching on index action of home controller that gets expired when user successfully logs in or out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   skip_before_filter :authorize
   
+  caches_action :index
+
   def index
     @person = Person.find(:first, :include => [:companies, :skills, :schools])
   end
