ec762a68176d3bbb8384c9dcc9e548ea49e6395a
Guy Newman <guy@weblitz.co.uk>
2010-07-30 11:50:11 +0100

    unpacked validation_reflections and formtastic gems

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "haml"
+  config.gem "formtastic"
+  config.gem "validation_reflection"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
