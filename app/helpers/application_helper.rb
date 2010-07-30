3891101aeba4cdde037ed45c65f39e3ac235b79e
Guy Newman <guy@weblitz.co.uk>
2010-07-30 13:03:44 +0100

    removed error_handling_form_builder along with helper and form templates as now using formtastic instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def error_handling_form_for(record_or_name_or_array, * args, & proc)
-    options = args.detect { |arg| arg.is_a?(Hash) }
-    if options.nil?
-      options = {:builder => ErrorHandlingFormBuilder}
-      args << options
-    end
-    options[:builder] = ErrorHandlingFormBuilder unless options.nil?
-    form_for record_or_name_or_array, * args, & proc
-  end
-
   def remove_child_link(name, form_builder, html_options = {})
     form_builder.hidden_field(:_destroy) + link_to(name, "javascript:void(0)", html_options)
   end
