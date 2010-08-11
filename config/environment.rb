6971fdc764d834c0ecb6c954728167d234d0c6ce
Guy Newman <guy@weblitz.co.uk>
2010-08-11 09:43:20 +0100

    configured and unpacked mocha and timecop gems

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,8 @@ Rails::Initializer.run do |config|
   config.gem "declarative_authorization"
   config.gem "machinist"
   config.gem "faker"
+  config.gem "mocha"
+  config.gem "timecop"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
