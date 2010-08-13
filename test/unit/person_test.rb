b7f89feb0bb1a2fb73e25e5a3724186b95d044da
Guy Newman <guy@weblitz.co.uk>
2010-08-13 18:20:10 +0100

    removed validation of photo for person. added person unit test stubs

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,8 +1,71 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "valid person" do
+    assert false
+  end
+
+  test "invalid person with missing name" do
+    assert false
+  end
+
+  test "invalid person with missing job_title" do
+    assert false
+  end
+
+  test "invalid person with missing phone" do
+    assert false
+  end
+
+  test "invalid person with missing mobile" do
+    assert false
+  end
+
+  test "invalid person with missing profile" do
+    assert false
+  end
+
+  test "invalid person with missing user" do
+    assert false
+  end
+
+  test "invalid person with taken name" do
+    assert false
+  end
+
+  test "invalid person with taken email" do
+    assert false
+  end
+
+  test "invalid person with wrong format url" do
+    assert false
+  end
+
+  test "invalid person with wrong format email" do
+    assert false
+  end
+
+  test "associated companies ordered by start date" do
+    assert false
+  end
+
+  test "associated skills ordered by list position" do
+    assert false
+  end
+
+  test "associated schools ordered by date from" do
+    assert false
+  end
+
+  test "accepts photo file upload" do
+    assert false
+  end
+
+  test "rejects photo file upload more than 10mb" do
+    assert false
+  end
+
+  test "rejects photo file upload unless an image" do
+    assert false
   end
 end
