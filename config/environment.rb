4765b52e5b2191426f92332aca342567f27cdbe7
Guy Newman <guy@weblitz.co.uk>
2010-08-17 11:37:54 +0100

    had to reconfigure compass and blueprints because didn't initialize rails support

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,7 @@ Rails::Initializer.run do |config|
   config.gem "faker"
   config.gem "mocha"
   config.gem "timecop"
+  config.gem "compass"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
