583c44193638d714dad8f3ee3864e15922cc9ef2
Guy Newman <guy@weblitz.co.uk>
2010-09-07 11:49:36 +0100

    only showing social media icons if there is a corresponding link. swapped pdf icon to front of list. only show project skills list if there is at least one skill defined

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100907071050) do
+ActiveRecord::Schema.define(:version => 20100907090243) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
